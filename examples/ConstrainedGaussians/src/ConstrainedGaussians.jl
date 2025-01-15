348975f62184db9519532ea809aac3a4829f3ab4
Uwe Hernandez Acosta <u.hernandez@hzdr.de>
2025-01-15 23:23:40 +0100

    added example

---
+++ examples/ConstrainedGaussians/src/ConstrainedGaussians.jl
@@ -0,0 +1,22 @@
+module ConstrainedGaussians
+
+
+using Distributions
+using DomainSets
+using Random
+using QuadGK
+using StatsPlots
+#using InclusiveScans
+
+export ConstrainedGaussian1D
+export target_dist,filter, norm, normalize
+export endpoints
+export generate_CPU
+export plot_compare
+
+include("interface.jl")
+include("impl.jl")
+include("generate.jl")
+include("plot_compare.jl")
+
+end
