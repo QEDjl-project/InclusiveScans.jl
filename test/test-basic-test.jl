74cdf178e636b18f902ed6c1b5ac59681c37d682
Anton Reinhard <anton.reinhard@proton.me>
2025-02-18 13:58:19 +0100

    Make main function generic (#10)

--- test/test-basic-test.jl
+++ test/test-basic-test.jl
@@ -3,28 +3,36 @@ using Random
 using CUDA
 using InclusiveScans
 
-@testset "InclusiveScans.jl Tests" begin
-    sizes = (1, 100, 1000, 25_000, 100_000)
+RNG = Xoshiro(137)  # Fixed seed
+SIZES = (1, 100, 1000, 25_000, 100_000)
+TYPES = (Float16, Float32, Float64, Int32, Int64, ComplexF32, ComplexF64)
 
-    for N in sizes
-        @testset "Test with N = $N" begin
-            rng = Xoshiro(137)  # Fixed seed
+_custom_eps(::Type{T}) where {T<:AbstractFloat} = sqrt(eps(T))
+_custom_eps(::Type{T}) where {T<:Integer} = zero(T)
+_custom_eps(::Type{Complex{T}}) where {T<:AbstractFloat} = sqrt(eps(T))
+
+@testset "InclusiveScans.jl Tests" begin
+    @testset "Test with T = $T" for T in TYPES
+        @testset "Test with N = $N" for N in SIZES
+            if (T == Float16 && N >= 10_000)
+                # precision of F16 is too little for large tests
+                continue
+            end
 
             # Generate random input
-            bit_input = BitVector(rand(rng, Bool, N))
-            float_input = Float32.(bit_input)
+            input = rand(RNG, T, N)
 
-            d_in = CuArray(float_input)
-            d_out = CUDA.zeros(Float32, N)
+            d_in = CuArray(input)
+            d_out = similar(d_in)
 
             # Run inclusive scan on GPU
             InclusiveScans.largeArrayScanInclusive!(d_out, d_in, Int32(N))
 
             h_out = Array(d_out)
-            h_check = Base.accumulate(+, float_input)
+            h_check = Base.accumulate(+, input)
 
             # Check if GPU result matches the CPU reference
-            @test isapprox(h_out, h_check; atol = eps(Float32))
+            @test isapprox(h_out, h_check, rtol = _custom_eps(T))
         end
     end
 end
